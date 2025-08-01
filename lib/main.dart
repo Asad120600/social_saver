bcc267df3923388f2d45070ab14f735dc487a797
Asad120600 <146355063+Asad120600@users.noreply.github.com>
2025-08-01 13:15:24 +0500

    old one

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:get/get.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:social_saver/views/splash_screen.dart';
-import 'package:social_saver/l10n/app_translations.dart'; 
+import 'package:social_saver/l10n/app_translations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,26 +25,24 @@ void main() async {
   );
   MobileAds.instance.initialize();
 
-  try {
-    await FlutterDownloader.initialize(debug: true);
-  } catch (e) {
-    print("FlutterDownloader Initialization Error: $e");
-  }
+  
+  
 
-
-  runApp(MyApp(initialLocale: initialLocale));}
+  runApp(MyApp(initialLocale: initialLocale));
+}
 
 class MyApp extends StatelessWidget {
   final Locale initialLocale;
 
   const MyApp({Key? key, required this.initialLocale}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       translations: AppTranslations(),
-      locale: Locale('en', 'US'), // default locale
-      fallbackLocale: Locale('en', 'US'),
+      locale: initialLocale, // ✅ Use the passed locale
+      fallbackLocale: const Locale('en', 'US'),
       title: 'QuickSaver - Video Downloader',
       theme: ThemeData(
         primarySwatch: Colors.blue,
